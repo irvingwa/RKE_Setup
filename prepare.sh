be3ec879eef64945da87cc36370322cb3a106a16
irvingwa <aaronirving4@gmail.com>
2020-02-29 04:24:15 -0800

    Create prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#nmtui
+yum -y install docker
+docker pull registry:2
+docker pull  nginx:1.14
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+curl -LO https://github.com/rancher/rke/releases/download/v1.0.4/rke_linux-amd64
+mv rke_linux-amd64 rke
+chmod +x rke
+sudo mv ./rke /usr/local/bin/rke
+curl -LO https://get.helm.sh/helm-v3.1.1-linux-amd64.tar.gz
+tar -zxvf helm-v3.0.0-linux-amd64.tar.gz
+chmod +x linux-amd64/helm
+mv linux-amd64/helm /usr/local/bin/helm
