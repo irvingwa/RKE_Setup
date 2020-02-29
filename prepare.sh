c5abbb7b3ab72aa26b1f7d60dc30001cab744540
irvingwa <aaronirving4@gmail.com>
2020-02-29 04:52:49 -0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -15,6 +15,17 @@ curl -LO https://get.helm.sh/helm-v3.1.1-linux-amd64.tar.gz
 tar -zxvf helm-v3.1.1-linux-amd64.tar.gz
 chmod +x linux-amd64/helm
 mv linux-amd64/helm /usr/local/bin/helm
+curl -LO https://raw.githubusercontent.com/irvingwa/RKE_Setup/master/nginx.conf
+mv nginx.conf /etc/nginx.conf
+echo Enter Node 1 IP
+read node1_ip
+sed -i -e 's/node1IP/$node1_ip/g' /etc/nginx.conf
+echo Enter Node 2 IP
+read node2_ip
+sed -i -e 's/node2IP/$node2_ip/g' /etc/nginx.conf
+echo Enter Node 3 IP
+read node3_ip
+sed -i -e 's/node3IP/$node3_ip/g' /etc/nginx.conf
 docker run -d -p 5000:5000 --name registry registry:2
 docker run -d --restart=unless-stopped \
   -p 80:80 -p 443:443 \
