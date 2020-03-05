c68f7801cef7141ed6f2d4b46197872b5f9b6f83
irvingwa <aaronirving4@gmail.com>
2020-03-05 09:28:31 -0500

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -13,8 +13,7 @@ read node3_ip
 
 yum -y install docker
 service docker start
-docker pull registry:2
-docker pull  nginx:1.14
+
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
@@ -26,19 +25,7 @@ curl -LO https://get.helm.sh/helm-v3.1.1-linux-amd64.tar.gz
 tar -zxvf helm-v3.1.1-linux-amd64.tar.gz
 chmod +x linux-amd64/helm
 mv linux-amd64/helm /usr/local/bin/helm
-curl -LO https://raw.githubusercontent.com/irvingwa/RKE_Setup/master/nginx.conf
-mv nginx.conf /etc/nginx.conf
 
-sed -i -e "s|node1IP|${node1_ip}|g" /etc/nginx.conf
-
-sed -i -e "s|node2IP|${node2_ip}|g" /etc/nginx.conf
-
-sed -i -e "s|node3IP|${node3_ip}|g" /etc/nginx.conf
-docker run -d -p 5000:5000 --name registry registry:2
-docker run -d --name nginx --restart=unless-stopped \
-  -p 80:80 -p 443:443 \
-  -v /etc/nginx.conf:/etc/nginx/nginx.conf \
-  nginx:1.14
 curl -LO https://github.com/rancher/rancher/releases/download/v2.3.5/rancher-images.txt
 curl -LO https://github.com/rancher/rancher/releases/download/v2.3.5/rancher-save-images.sh
 curl -LO https://github.com/rancher/rancher/releases/download/v2.3.5/rancher-load-images.sh
